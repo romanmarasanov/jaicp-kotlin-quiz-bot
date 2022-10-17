04a93a815df1de6f9ec21c97976da3c0535a8c31
Roman Marasanov <Roman_Marasanov@epam.com>
2022-10-18 01:44:17 +0300

    change TemplateBot: remove redundant CAILA settings (have worked with regex only), IntentActivator (for the same reason) and leave only one logger

--- src/main/kotlin/com/justai/jaicf/template/TemplateBot.kt
+++ src/main/kotlin/com/justai/jaicf/template/TemplateBot.kt
@@ -1,10 +1,8 @@
 package com.justai.jaicf.template
 
 import com.justai.jaicf.BotEngine
-import com.justai.jaicf.activator.caila.CailaIntentActivator
 import com.justai.jaicf.activator.caila.CailaNLUSettings
 import com.justai.jaicf.activator.regex.RegexActivator
-import com.justai.jaicf.channel.jaicp.logging.JaicpConversationLogger
 import com.justai.jaicf.logging.Slf4jConversationLogger
 import com.justai.jaicf.template.scenario.mainScenario
 import java.util.*
@@ -14,18 +12,12 @@ val accessToken: String = System.getenv("JAICP_API_TOKEN") ?: Properties().run {
     getProperty("apiToken")
 }
 
-private val cailaNLUSettings = CailaNLUSettings(
-    accessToken = accessToken
-)
-
 val templateBot = BotEngine(
     scenario = mainScenario,
     conversationLoggers = arrayOf(
-        JaicpConversationLogger(accessToken),
         Slf4jConversationLogger()
     ),
     activators = arrayOf(
-        CailaIntentActivator.Factory(cailaNLUSettings),
         RegexActivator
     )
 )
